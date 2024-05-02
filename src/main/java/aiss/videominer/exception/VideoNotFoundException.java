2f9a323e7edaecfc92c4352d61d2c208a9eeb69b
SamTBalogh <samtambal@alum.us.es>
2024-05-02 17:55:05 +0200

    Controllers updated, Exceptions added, Token in production

---
+++ src/main/java/aiss/videominer/exception/VideoNotFoundException.java
@@ -0,0 +1,8 @@
+package aiss.videominer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "Videos not found with that Channel Id")
+public class VideoNotFoundException extends Exception{
+}
