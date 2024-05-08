08cfb8d0df57fb3cdde66d9014ffd87d32d2c1b6
SamTBalogh <samtambal@alum.us.es>
2024-05-08 17:26:51 +0200

    Controller bug fix

--- src/main/java/aiss/videominer/controller/ChannelController.java
+++ src/main/java/aiss/videominer/controller/ChannelController.java
@@ -2,9 +2,7 @@ package aiss.videominer.controller;
 
 
 import aiss.videominer.exception.*;
-import aiss.videominer.model.Comment;
 import aiss.videominer.model.Channel;
-import aiss.videominer.model.Video;
 import aiss.videominer.repository.CaptionRepository;
 import aiss.videominer.repository.ChannelRepository;
 import aiss.videominer.repository.CommentRepository;
@@ -163,16 +161,6 @@ public class ChannelController {
                 throw new IdCannotBeNull();
             }
             Channel _channel = channelRepository.save(channel);
-            for (Video v : channel.getVideos()) {
-                Video video = videoRepository.save(v);
-                _channel.getVideos().add(video);
-                captionRepository.saveAll(v.getCaptions());
-                for (Comment com : v.getComments()) {
-                    Comment comment = commentRepository.save(com);
-                    video.getComments().add(comment);
-                    userRepository.save(comment.getAuthor());
-                }
-            }
             return _channel;
         } else {
             throw new TokenNotValidException();
